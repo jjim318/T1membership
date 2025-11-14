201cc85ff41ab890373fd975dce5b7a4c57e3cc4
220-04\wlals <wow31844@gmail.com>
2025-11-14 14:41:31 +0900

    컨트롤러 완

--- src/main/java/com/t1membership/auth/controller/AuthController.java
+++ src/main/java/com/t1membership/auth/controller/AuthController.java
@@ -1,15 +1,52 @@
 package com.t1membership.auth.controller;
 
+import com.t1membership.auth.dto.loginDto.LoginReq;
+import com.t1membership.auth.dto.tokenDto.TokenReq;
+import com.t1membership.auth.dto.tokenDto.TokenRes;
+import com.t1membership.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping("/login")
-    public R
+    public ResponseEntity<TokenRes> login(@RequestBody LoginReq loginReq) {
+        return ResponseEntity.ok(authService.login(loginReq));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenRes> refresh(@RequestBody TokenReq tokenReq) {
+        return ResponseEntity.ok(authService.refresh(tokenReq));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<TokenRes> logout(@RequestHeader(value = "AuthorIzation",required = false) String authorization,
+    @RequestBody TokenReq tokenReq) {
+        String access = clean(authorization);
+        String refresh = clean(tokenReq.getRefreshToken());
+
+        TokenReq req = TokenReq.builder()
+                .accessToken(access)
+                .refreshToken(refresh)
+                .build();
+
+        authService.logout(req);
+        return ResponseEntity.noContent().build();
+    }
+
+    private String clean(String token) {
+        if (token == null) return null;
+        token = token.trim();
+        if (token.isBlank()) return null;
+        if (token.toLowerCase().startsWith("bearer ")) {
+            return token.substring(7).trim();
+        }
+        return token;
+    }
 }
